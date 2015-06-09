0188cafb14cef307972ee64819fb79ef6809bd11
Carlos Duran <afg504@gmail.com>
2015-06-09 16:44:51 -0400

    Reinstated constraint changes

--- AdaptiveWeather/ViewController.swift
+++ AdaptiveWeather/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // * some changes will go here
     }
 
     override func didReceiveMemoryWarning() {
